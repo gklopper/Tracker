7f0ef3d13f110aaec0f4c2b4f2c6e2dc6104d605
Grant Klopper <grant.klopper@gmail.com>
2010-12-04 14:36:39 +0000

    When a story is created it will now automatically have a comment posted with a link to the tracker

--- pivotaltracker.py
+++ pivotaltracker.py
@@ -1,21 +1,36 @@
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp import util
-from xml.dom import minidom
-from models import Story
+from google.appengine.api import urlfetch
+from models import Story, Project
 import logging
 from event_parser import EventParser
-import urllib2
+import os
+
 
 
 class Handler(webapp.RequestHandler):
+
+
 
     def _create_new_story(self, event):
         logging.info('Creating story')
         story = Story(story_id=event.story_id, name=event.story_title, )
         story.put()
 
-        #post a comment back to pivotal tracker
+        project = Project.get()
 
+        api_url = 'http://www.pivotaltracker.com/services/v3/projects/{PROJECT_ID}/stories/{STORY_ID}/notes'.replace('{PROJECT_ID}', project.project_id).replace('{STORY_ID}', story.story_id)
+        comment_xml = '<note><text>http://{APP_ID}.appspot.com/s/{STORY_ID}</text></note>'.replace('{APP_ID}', os.environ['APPLICATION_ID']).replace('{STORY_ID}', story.story_id)
+
+        logging.info('Api url: ' + api_url)
+        logging.info('Comment xml: ' + comment_xml)
+
+        #post a comment back to pivotal tracker
+        result = urlfetch.fetch(url=api_url,
+                                payload=comment_xml,
+                                method=urlfetch.POST,
+                                headers={'Content-Type': 'application/xml', 'X-TrackerToken': project.api_token})
+        logging.info('Result of API call: ' + result.content)
 
     def _accept_story(self, event):
         logging.info('Accepting story: ' + event.story_id)
